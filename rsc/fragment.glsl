85281e6b2beac508ba90711e40d51199b636809c
Rj Rybarczyk <rj@rybar.tech>
2020-03-21 12:14:03 -0400

    Make right half of screen black

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -8,5 +8,9 @@ in vec2 uv;
 out vec4 Target0;
 
 void main() {
-  Target0 = vec4(uv, 1.0, 1.0);
+  if (uv.x > 0.5) {
+    Target0 = vec4(0.0, 0.0, 0.0, 1.0);
+  } else {
+    Target0 = vec4(uv, 1.0, 1.0);
+  }
 }
