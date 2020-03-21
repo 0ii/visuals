4260fadde7fd236c996b2bd479e0e58d6a1b4636
Rj Rybarczyk <rj@rybar.tech>
2020-03-21 12:54:29 -0400

    Color circle outside black

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -8,7 +8,10 @@ in vec2 uv;
 out vec4 Target0;
 
 void main() {
-  if (uv.x > 0.5) {
+  vec2 c = vec2(0.5, 0.5);
+  float radius = 0.1;
+  float dist = length(uv - c);
+  if (dist > radius) {
     Target0 = vec4(0.0, 0.0, 0.0, 1.0);
   } else {
     Target0 = vec4(uv, 1.0, 1.0);
