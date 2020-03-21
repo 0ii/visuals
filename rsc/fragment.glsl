b05c3bfe0fe310df5b99bea58521e9737b0be1b6
Rj Rybarczyk <rj@rybar.tech>
2020-03-21 12:10:44 -0400

    Cyan and magenta fade

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -4,9 +4,9 @@ layout (std140) uniform Uniforms {
     float time;
 };
 
+in vec2 uv;
 out vec4 Target0;
 
 void main() {
-  // ignore everything about color and use white
-  Target0 = vec4(vec3(1.0, 1.0, 1.0)  * (cos(time) * 0.5 + 0.5), 1.0);
+  Target0 = vec4(uv, 1.0, 1.0);
 }
--- rsc/vertex.glsl
+++ rsc/vertex.glsl
@@ -1,7 +1,9 @@
 #version 150 core
 
 in vec2 a_Pos;
+out vec2 uv;
 
 void main() {
-    gl_Position = vec4(a_Pos, 0.0, 1.0);
+  uv = a_Pos * 0.5 + 0.5;
+  gl_Position = vec4(a_Pos, 0.0, 1.0);
 }
