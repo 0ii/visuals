be0c9ebabdf78c7d59b3c28f01f77a38f103a811
Rj Rybarczyk <rj@rybar.tech>
2020-03-21 14:43:26 -0400

    Pink dimming circle, purple background

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -8,12 +8,14 @@ in vec2 uv;
 out vec4 Target0;
 
 void main() {
+  vec4 color1 = vec4(0.5, 0.2, 1.0, 1.0); // RGBA color
+  vec4 color2 = color1.abgr;
   vec2 c = vec2(0.5, 0.5);
   float radius = 0.1;
   float dist = length(uv - c);
   if (dist > radius) {
-    Target0 = vec4(0.0, 0.0, 0.0, 1.0);
+    Target0 = color1;
   } else {
-    Target0 = vec4(uv, 1.0, 1.0);
+    Target0 = vec4(color2.bgr  * (cos(time) * 0.5 + 0.5), 0.5);
   }
 }
