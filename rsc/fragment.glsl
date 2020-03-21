1d76e802f078c9e3801660102b9cdfcf4e709537
Rj Rybarczyk <rj@rybar.tech>
2020-03-21 15:07:46 -0400

    Dimming left

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -8,14 +8,22 @@ in vec2 uv;
 out vec4 Target0;
 
 void main() {
+
+  vec2 a = vec2(0.3, 0.7);
   vec4 color1 = vec4(0.5, 0.2, 1.0, 1.0); // RGBA color
   vec4 color2 = color1.abgr;
-  vec2 c = vec2(0.5, 0.5);
-  float radius = 0.1;
-  float dist = length(uv - c);
-  if (dist > radius) {
+  if (uv.x > a.x) {
     Target0 = color1;
   } else {
     Target0 = vec4(color2.bgr  * (cos(time) * 0.5 + 0.5), 0.5);
   }
+  //
+  // vec2 c = vec2(0.5, 0.5);
+  // float radius = 0.1;
+  // float dist = length(uv - c);
+  // if (dist > radius) {
+  //   Target0 = color1;
+  // } else {
+  //   Target0 = vec4(color2.bgr  * (cos(time) * 0.5 + 0.5), 0.5);
+  // }
 }
