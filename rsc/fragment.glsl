ee48d211452c1f538fdb90092723efe980f3b7d2
Rj Rybarczyk <rj@rybar.tech>
2020-03-21 11:39:23 -0400

    Clean up

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -1,30 +1,12 @@
 #version 150 core
 
-uniform sampler2D t_Texture;
-in vec2 v_Uv;
-in vec4 v_Color;
-out vec4 Target0;
-
-layout (std140) uniform Globals {
-    mat4 u_MVP;
-};
-
 layout (std140) uniform Dim {
     float u_Rate;
 };
 
-void main() {
-  //texture function takes a
-  //  1. sampler - thing that holds a texture that you can read pixels out of it
-  //  2. u,v coord - coord to read out of that texture
-  // v_Color - input from vertex shader
-  // u Rate - a uniform, it is a uniform input that is the same for all vertices. a constant
-  // each frame gets a new u_Rate, but is constant on a per frame basis
-    Target0 = texture(t_Texture, v_Uv) * v_Color * u_Rate;
-    Target0.a = 1.0;
-}
+out vec4 Target0;
 
-// a candidate or potential pixel
-// pixels are rgb values in the buffer
-// your going to get fragments and combine them int eh buffer by the compositor
-// the alpha value determines how the compositor, blends that frag into a final actual pixel
+void main() {
+  // ignore everything about color and use white
+  Target0 = vec4(vec3(1.0, 1.0, 1.0)  * u_Rate, 1.0);
+}
--- rsc/vertex.glsl
+++ rsc/vertex.glsl
@@ -1,35 +1,7 @@
 #version 150 core
 
 in vec2 a_Pos;
-in vec2 a_Uv;
 
-in vec4 a_Src;
-in vec4 a_TCol1;
-in vec4 a_TCol2;
-in vec4 a_TCol3;
-in vec4 a_TCol4;
-in vec4 a_Color;
-
-layout (std140) uniform Globals {
-    mat4 u_MVP;
-};
-
-out vec2 v_Uv;    // coordinates in texture space
-out vec4 v_Color;
-
-// 1. compute gl_Position
-// 2. compute any special outputs needed by the fragment(pixel) shader
-// can caluclate any date you want in frag shader, or calc in vertex and pass it to the frag shader
-// frag shader runs more times than the vertex shader
 void main() {
-    v_Uv = a_Uv * a_Src.zw + a_Src.xy;    // doing the texture coord calc
-    v_Color = a_Color;  // passing color straight through to vertex shader (a is a per vertex attribute)
-    mat4 instance_transform = mat4(a_TCol1, a_TCol2, a_TCol3, a_TCol4);
-    vec4 position = instance_transform * vec4(a_Pos, 0.0, 1.0);
-
-    // gl_Position = u_MVP * position;   // special var, where the transformed vertex is
     gl_Position = vec4(a_Pos, 0.0, 1.0);
-    // z - 0
-    // homogenous coord - add w component that is used somehow to make projections work. 1 is the normal value
-
 }
