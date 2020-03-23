dc7c158e6044115525157c0bd4de3b203d65e9a6
RJ Rybarczyk <rj@rybar.tech>
2020-03-23 03:30:24 +0000

    Zoom out quarter circle square

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -16,12 +16,18 @@ bool lower(vec2 v) {
   return v.y < 0.5;
 }
 
+
+bool quarter_circle(vec2 v) {
+  float radius = 1.0;
+  return length(v) < radius;
+}
+
 void main() {
   vec4 color1 = vec4(0.5, 0.2, 1.0, 1.0); // RGBA color
   vec4 color2 = color1.abgr;
 
   vec2 v = mod(uv * time, 1.0);
-  if (left(v) && lower(v)) {
+  if (!(left(v) && lower(v)) && quarter_circle(v)) {
     // Point is on the inside
     Target0 = vec4(color2.bgr  * (cos(time) * 0.5 + 0.5), 0.5);
   } else {
