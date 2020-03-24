ddb185f72051cfc3b85811483b26e44d81cbe26a
RJ Rybarczyk <rj@rybar.tech>
2020-03-24 02:10:47 +0000

    Add in noise and some green

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -7,6 +7,14 @@ layout (std140) uniform Uniforms {
 in vec2 uv;
 out vec4 Target0;
 
+float rand2D(in vec2 co){
+      return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+float rand3D(in vec3 co){
+      return fract(sin(dot(co.xyz ,vec3(12.9898,78.233,144.7272))) * 43758.5453);
+}
+
 
 bool left(vec2 v) {
   return v.x < 0.5;
@@ -20,12 +28,17 @@ void main() {
   vec4 color1 = vec4(0.5, 0.2, 1.0, 1.0); // RGBA color
   vec4 color2 = color1.abgr;
 
+  float test = rand2D(uv);
+  // Target0 = vec4(test, color1.rg, 1.0);
+
   vec2 v = mod(uv * time, 1.0);
+  vec3 t = vec3(test, color1.rg);
   if (left(v) && lower(v)) {
     // Point is on the inside
-    Target0 = vec4(color2.bgr  * (cos(time) * 0.5 + 0.5), 0.5);
+    Target0 = vec4(t, 0.5);
   } else {
     // Point is on the outside
-    Target0 = color1;
+    Target0 = vec4(test, color1.rg, 1.0);
+    // Target0 = color1;
   }
 }
