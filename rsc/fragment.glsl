d023291733588fcad6469c6075ee0126a4d64a04
RJ Rybarczyk <rj@rybar.tech>
2020-03-26 19:09:13 +0000

    2x2 checkerboard

--- rsc/fragment.glsl
+++ rsc/fragment.glsl
@@ -7,40 +7,34 @@ layout (std140) uniform Uniforms {
 in vec2 uv;
 out vec4 Target0;
 
-float rand2D(in vec2 co){
-      return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-}
-
-float rand3D(in vec3 co){
-      return fract(sin(dot(co.xyz ,vec3(12.9898,78.233,144.7272))) * 43758.5453);
-}
-
-
 bool left(vec2 v) {
   return v.x < 0.5;
+}
+
+bool right(vec2 v) {
+  return v.x > 0.5;
 }
 
 bool lower(vec2 v) {
   return v.y < 0.5;
 }
 
+bool upper(vec2 v) {
+  return v.y > 0.5;
+}
+
 void main() {
   vec4 color1 = vec4(0.5, 0.2, 1.0, 1.0); // RGBA color
   vec4 color2 = color1.abgr;
 
-  float r2d = rand2D(uv);
-  vec3 gg = vec3(uv, r2d);
-  float r3d = rand3D(gg);
-  // Target0 = vec4(r2d, color1.rg, 1.0);
-
-  vec2 v = mod(uv * time, 1.0);
-  vec3 t = vec3(r2d, color1.rg);
-  if (left(v) && lower(v)) {
+  if (left(uv) && lower(uv)) {
     // Point is on the inside
-    Target0 = vec4(t, 0.5);
+    Target0 = color1;
+  } else if (right(uv) && upper(uv)) {
+    // Point is on the outside
+    Target0 = color1;
   } else {
     // Point is on the outside
-    Target0 = vec4(r3d, color1.rg, 1.0);
-    // Target0 = color1;
+    Target0 = color2;
   }
 }
